71ee3b2f0c19abf6a18c0c3b3a8d8e86b1a4b46d
eviw817 <evelien.derudder@gmail.com>
2025-01-29 13:57:35 +0100

    feat: add SafeAreaView and change padding

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,22 +1,23 @@
-import { View } from 'react-native';
 import { Link } from 'expo-router';
+import { View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function HomeScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#FFFDF9',
-        }}>
-        <Link 
+    <SafeAreaView style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#FFFDF9',
+    }}>
+      <Link 
         style={{
-          padding: 8,
+          padding: 12,
+          paddingHorizontal: 20,
           backgroundColor: '#97B8A5',
-          borderRadius: 7,
-        }}
-        href="/demo">Ga naar demo</Link>
-      </View>
+          borderRadius: 7 }}
+      href="/demo">Ga naar demo</Link>
+    </SafeAreaView>
+    
   );
 }
