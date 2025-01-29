70f76069e360a3fe493c5ef5bae4255cbef2d132
eviw817 <evelien.derudder@gmail.com>
2025-01-29 12:26:51 +0100

    feat: try to see dog (working on it)

--- app/ardog.tsx
+++ app/ardog.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { ViroARScene, Viro3DObject } from "@viro-community/react-viro";
 
-const dogModel = require('../assets/3d/dog.glb');
+const dogModel = require('../assets/3d/pitbull.glb');
 
 const MyARScene: React.FC = () => {
   return (
     <ViroARScene>
       <Viro3DObject
         source={dogModel} // Locatie van het 3D-model
-        position={[0, 0, -2]} // Plaatsing in de 3D-ruimte (x, y, z)
-        scale={[1, 1, 1]} // Grootte van het object
+        position={[0, 0, -2]} // Plaatsing in de 3D-ruimte, iets verder van de camera
+        scale={[2, 2, 2]}  // Grootte van het object
         type="GLB"
       />
     </ViroARScene>
