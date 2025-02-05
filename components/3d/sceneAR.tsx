fb5d7fbc72d191d4760df2367ba7200aea8b0156
eviw817 <evelien.derudder@gmail.com>
2025-02-05 19:16:06 +0100

    feat: Able to show dog in AR

--- components/3d/sceneAR.tsx
+++ components/3d/sceneAR.tsx
@@ -45,19 +45,17 @@ import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import {
   ViroARScene,
-  ViroText,
   ViroTrackingStateConstants,
   ViroARSceneNavigator,
   ViroTrackingReason,
+  Viro3DObject,
+  ViroAmbientLight,
 } from "@reactvision/react-viro";
 
 const DogSceneAR = () => {
-  const [text, setText] = useState("Initializing AR...");
-
   function onInitialized(state: any, reason: ViroTrackingReason) {
     console.log("guncelleme", state, reason);
     if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
-      setText("Hello World!");
     } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
       // Handle loss of tracking
     }
@@ -65,11 +63,17 @@ const DogSceneAR = () => {
 
   return (
     <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
+      <ViroAmbientLight color="#FFFFFF" />
+      <Viro3DObject
+        source={require("../../assets/3d/pitbull.glb")}
+        type="GLB"
+        scale={[ 1, 1, 1 ]}
+        position={[ 0, 0, 0 ]}
+        onLoadStart={() => console.log("Loading object...")}
+        onLoadEnd={() => console.log("Finished Loading object")}
+        onError={({ nativeEvent }) =>
+          console.log("Damn, something went wrong: ", nativeEvent)
+        }
       />
     </ViroARScene>
   );
