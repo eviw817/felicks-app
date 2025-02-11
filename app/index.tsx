8031c00995d0fbe6e41b1b27cb268946d9386c29
IsabelDH <isabel.de.haes@hotmail.com>
2025-02-11 21:13:59 +0100

    feat: login_register.tsx add

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,37 @@
-import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import React, { useEffect } from "react";
+import { View, Image, StyleSheet } from "react-native";
+import { useRouter } from "expo-router"; 
 
 export default function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/login_register"); 
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <View style={styles.container} className="bg-baby-powder">
-    <Image
-      source={require('../assets/images/logo_felicks.png')} 
-      style={styles.logo}
-    />
-  </View>
+      <Image
+        source={require("../assets/images/logo_felicks.png")}
+        style={styles.logo}
+      />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   logo: {
     width: 340,
     height: 340,
-    resizeMode: 'contain',
+    resizeMode: "contain",
   },
 });
-
---
+++ app/login_register.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+
+function LoginRegisterScreen() {
+  return (
+    <View style={styles.container} className="bg-baby-powder">
+      <Image 
+        source={require("../assets/images/logo_felicks.png")}
+        style={styles.logo}
+      />
+      <Text style={styles.title} className='semibold'>Welkom bij Felicks!</Text>
+      <Text style={styles.subtitle}>
+        Ontdek of een hond bij jouw levensstijl past. Felicks helpt je met het maken van een weloverwogen keuze!
+      </Text>
+      <TouchableOpacity style={styles.button} >
+        <Text style={styles.buttonText}>INLOGGEN</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>REGISTREREN</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  logo: {
+    width: 280, 
+    height: 280,
+    resizeMode: "contain",
+    marginBottom: -50, 
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#183A36',
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#183A36',
+    marginBottom: 50,
+    paddingHorizontal: 30, 
+  },
+  button: {
+    backgroundColor: '#97B8A5',
+    paddingVertical: 15,
+    borderRadius: 20, 
+    marginBottom: 20,
+    width: '95%',
+    alignItems: 'center',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
+  buttonText: {
+    color: '#183A36',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default LoginRegisterScreen;
