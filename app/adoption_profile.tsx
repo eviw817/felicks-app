b8ad1b1c6cbc1a37a96af8434431f43f8a8a03b5
Hannelorevb04 <hannelorevb@hotmail.be>
2025-02-12 21:23:21 +0100

    feat: adoptieprofiel na registratie

---
+++ app/adoption_profile.tsx
@@ -0,0 +1,100 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { useRouter } from 'expo-router'; 
+import { Ionicons } from '@expo/vector-icons';
+
+function AdoptionProfileScreen() {
+  const router = useRouter(); 
+
+  return (
+    <View style={styles.container} className="bg-baby-powder">
+
+      {/* pijltje*/}
+      <TouchableOpacity style={styles.backButton} onPress={() => router.back()}>
+        <Ionicons name="arrow-back" size={24} color="#183A36" />
+      </TouchableOpacity>
+
+      {/* titel */}
+      <Text style={styles.title}>Adoptieprofiel</Text>
+      
+      {/* text */}
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>
+          Bedankt voor je registratie! Maak nu een adoptieprofiel aan om te ontdekken welke hond bij jou past.
+        </Text>
+        <Text style={styles.text}>
+          Vul je voorkeuren en gewoontes in en ontvang gepersonaliseerde aanbevelingen. Start vandaag nog en maak een weloverwogen keuze voor een nieuwe huisgenoot.
+        </Text>
+      </View>
+
+      {/* buttons */}
+      <TouchableOpacity style={styles.button} >
+        <Text style={styles.buttonText}>adoptieprofiel opzetten</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>overslaan</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 20,
+    paddingTop: 50,
+  },
+
+  backButton: {
+    position: 'absolute',
+    top: 50,
+    left: 20, 
+    zIndex: 10,
+  },
+
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#183A36',
+    marginBottom: 50,
+    textAlign: 'center',
+    alignSelf: 'center',
+    fontFamily:'../assets/fonts/SpaceMono-Regular.ttf',
+  },
+
+  textContainer: {
+    marginBottom: 50, // alleen ruimte tussen de tekst en de buttons
+  },
+
+  text: {
+    fontSize: 16,
+    color: '#183A36',
+    marginBottom: 15,
+  },
+
+  button: {
+    backgroundColor: '#97B8A5',
+    paddingVertical: 15,
+    borderRadius: 20, 
+    marginBottom: 20,
+    width: '100%', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
+  
+  buttonText: {
+    color: '#183A36',
+    fontSize: 14,
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    width: '100%',
+  },
+});
+
+export default AdoptionProfileScreen;
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,37 @@
-import { Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { View, Image, StyleSheet } from "react-native";
+import { useRouter } from "expo-router"; 
 
 export default function Index() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/adoption_profile");  // ✅ Gebruik de route als string
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container} className="bg-baby-powder">
+      <Image
+        source={require("../assets/images/logo_felicks.png")}
+        style={styles.logo}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    width: 340,
+    height: 340,
+    resizeMode: "contain",
+  },
+});
