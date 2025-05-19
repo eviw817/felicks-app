48e811222325dc2ba7d9bbf0771815020d9e135d
EvelienDR <evelien.d@hybridmedia.be>
2025-05-19 14:52:20 +0200

    no errors, dog is visible, everything works

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,13 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+if (!config.resolver) {
+  config.resolver = {};
+}
+if (!config.resolver.assetExts) {
+  config.resolver.assetExts = [];
+}
+
+config.resolver.assetExts.push('glb');
+
 module.exports = withNativeWind(config, { input: "./global.css" });
